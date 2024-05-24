a002065bcc32c66f97b61c46e755681b5903e275
eduudecebalcuza <162722197+eduudecebalcuza@users.noreply.github.com>
2024-05-23 21:40:38 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,33 @@
 def index_of_by_index(word, list, index):
+    for i in range(index, len(list)):
+        if list[i] == word:
+            return i
     return -1
-
 
 def index_of_empty(list):
+    for i, index in enumerate(list):
+        if index == "":
+            return i
     return -1
-
+print(index_of_empty(colors))
 
 def index_of(word, list):
+    for i, index in enumerate(list):
+        if index == word:
+            return i
     return -1
-
 
 def put(word, list):
+    for i in range(len(list)):
+        if list[i] == "":
+            list[i] = word
+            return i
     return -1
-
 
 def remove(word, list):
-    return -1
+    eliminate = 0
+    for i in range(len(list)):
+        if list[i] == word:
+            list[i] = ""
+            eliminate += 1
+    return eliminate
